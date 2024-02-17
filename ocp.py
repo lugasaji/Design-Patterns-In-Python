56cbd94c9c5669a1c5bd26256858e5d149d30c09
Gabriel Santisteban <gabriel.santisteban.dev@gmail.com>
2024-02-17 22:49:06 +0100

    OCP don't violated

---
+++ ocp.py
@@ -0,0 +1,34 @@
+# OPEN CLOSE PRINCIPLE: open for extensios and close for  modification
+
+from enum import Enum
+from typing import List
+
+
+class Color(Enum):
+    RED = 1
+    GREEN = 2
+    BLUE = 3
+
+class Size(Enum):
+    SMALL = 1
+    MEDIUM = 2
+    LARGE = 3
+
+class Product:
+
+    def __init__(self, name: str, color: Color, size: Size):
+        self.name = name
+        self.color = color
+        self.size = size
+    
+
+
+class ProductFilter:
+    def filter_by_color(self, products: List[Product], color: Color):
+        for p in products:
+            if p.color == color: 
+                yield p
+
+
+
+                
